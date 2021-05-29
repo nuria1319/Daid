1143f10b74c4abaee57de8886907f614e5a27531
unknown <nuriafernandez1319@gmail.com>
2021-05-29 21:18:44 +0200

    primer commit Daid

---
+++ src/test/java/com/D/Daid/DaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D.Daid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
