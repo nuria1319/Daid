5bd8f3252bfa8e33dd2f600a45ddd9f76993064f
unknown <nuriafernandez1319@gmail.com>
2021-06-02 12:14:48 +0200

    Subo código fix

--- src/main/java/com/D/Daid/models/DaidModel.java
+++ src/main/java/com/D/Daid/models/DaidModel.java
@@ -1,7 +1,6 @@
 package com.D.Daid.models;
 
-import java.sql.Date;
-
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
